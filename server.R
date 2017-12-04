d32c31563b2c8013a70744ec38b51de6f9c6c2cc
ByungSuJung <jbsoo93@gmail.com>
2017-12-03 21:38:12 -0800

    Create Scatter Plot

--- server.R
+++ server.R
@@ -1,28 +1,54 @@
 library(shiny)
 library(dplyr)
-library(rsconnect)
+
 library(plotly)
 
 # Read the csv file of FIFA 2018 soccer player dataset
-raw.data <- read.csv("./Data/complete.csv", stringsAsFactors = FALSE)
+raw.data <- read.csv("./Data/complete.csv", stringsAsFactors = FALSE, encoding = 'UTF-8')
 
-data <- raw.data %>% 
-  filter(overall >= 70)
-# Filter the FIFA 2018 player data by overall
-data.filter <- function(my_overall) {
-  filter(raw.data, overall >= my_overall)
-}
+# Name of positions
+positions <- select(raw.data, contains("prefers_")) %>% 
+  colnames()
 
+# Column names with numerical data in raw.data
+raw.numerical.col.names <- raw.data %>% 
+  select_if(is.numeric) %>% 
+  colnames()
+
+numeric.col.names <- raw.numerical.col.names[-c(1,2)]
+
+
+# Shiny server
 shinyServer(function(input, output) {
-  ouput$playerPlot <- renderPlotly({
+  output$playerPlot <- renderPlotly({
+
+    # Filter the FIFA 2018 player data by overall
+    filtered.data <-  filter(raw.data, overall >= as.numeric(input$overall))
     
-    filtered.data <- data.filter(input$overall)
+    # Create Linear Regression 
+    fit <- lm(filtered.data, formula = get(input$yaxis) ~ get(input$xaxis))
     
+    output$summary <- renderText({
+      return(paste0("Slope : ", round(fit$coefficients[[2]], 3)))
+    })
+    
+    
+    
+    # Draw scatter plot
     plot_ly(data = filtered.data,
-            type = 'scatter',
-            x =
-            )
+                 type = 'scatter',
+                 mode = 'markers',
+                 x = ~get(input$xaxis),
+                 y = ~get(input$yaxis),
+                 color = ~get(input$categorize)
+            ) %>% 
       
+      add_trace(data = filtered.data, x = ~get(input$xaxis), y = ~fitted(fit), mode = 'lines', color = NULL, name = "Linear Regression") %>% 
       
+      layout(title = paste0(input$xaxis, " V.S ", input$yaxis),
+             xaxis = list(title = input$xaxis),
+             yaxis = list(title = input$yaxis)
+      )
   })
+
 })
--- ui.R
+++ ui.R
@@ -1,6 +1,36 @@
-library(shiny)
+library(shiny)  
 
-shinyUI(navbarPage("Applications",
-                   tabPanel()
+source("server.R")
+shinyUI(fluidPage(
   
+  titlePanel("TITLE"),
+  
+  p("description"),
+
+  mainPanel(
+    
+    plotlyOutput("playerPlot"),
+    
+    textOutput("summary")
+    
+  ),
+  
+  sidebarPanel(
+    width = 2,
+    selectInput("overall", "Select minimum overall stat of the players",
+                choices = list("90","80","70","60"),
+                selected = "90"),
+    
+    selectInput("xaxis", "Select X-axis",
+                choices = numeric.col.names,
+                selected = "age"),
+    
+    selectInput("yaxis", "Select Y-axis",
+                choices = numeric.col.names,
+                selected = "eur_wage"),
+    selectInput("categorize", "Categorize by",
+                choices = c("league","club"),
+                selected = "league")
+    
+  )
 ))
