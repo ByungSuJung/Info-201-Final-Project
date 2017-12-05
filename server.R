6f2350f82ba9b471fa81608cd26f0530ded123b9
ByungSuJung <jbsoo93@gmail.com>
2017-12-05 13:51:17 -0800

    Pull

--- server.R
+++ server.R
@@ -1,36 +1,12 @@
 library(shiny)
 library(dplyr)
-library(plyr)
+
 library(plotly)
 
 # Read the csv file of FIFA 2018 soccer player dataset
 raw.data <- read.csv("./Data/complete.csv", stringsAsFactors = FALSE, encoding = 'UTF-8')
 
-# Changing the column names to be more user friendly
-raw.data <- rename(raw.data, c("club"="Club", "age"="Age", "league"="League", "height_cm"="Height(cm)",
-                               "weight_kg"="Weight(kg)", "eur_value"="Value(EUR)","eur_wage"="Wage(EUR)",
-                               "eur_release_clause"="Release Clause(EUR)", "overall"="Overall", "potential"
-                               ="Potential", "pac"="Pace", "sho"="Shooting", "pas"="Passing", "dri"="Dribbling",
-                               "def"="Defense", "phy"="Physical", "international_reputation"="International 
-                               Reputation", "skill_moves"="Skill Moves", "weak_foot"="Weak Foot", "crossing"=
-                               "Crossing", "finishing"="Finishing", "heading_accuracy"="Heading Accuracy",
-                               "short_passing"="Short Passing", "volleys"="Volleys", "dribbling"="Dribbling Skill",
-                               "curve"="Curve", "free_kick_accuracy"="Free Kick Accuracy", "long_passing"="Long Passing",
-                               "ball_control"="Ball Control", "acceleration"="Acceleration", "sprint_speed"="Sprint Speed",
-                               "agility"="Agility", "reactions"="Reactions", "balance"="Balance", "shot_power"="Shot Power",
-                               "jumping"="Jumping", "stamina"="Stamina", "strength"="Strength", "long_shots"="Long Shots",
-                               "aggression"="Aggression", "interceptions"="Interceptions", "positioning"="Positioning",
-                               "vision"="Vision", "penalties"="Penalties", "composure"="Composure", "marking"="Marking",
-                               "standing_tackle"="Standing Tackle", "sliding_tackle"="Sliding Tackle", "gk_diving"="Goalkeeper Diving",
-                               "gk_handling"="Goalkeeper Handling", "gk_kicking"="Goalkeeper Kicking", "gk_positioning"=
-                               "Goalkeeper Positioning", "gk_reflexes"="Goalkeeper Reflexes", "rs"="Right Striker", "rw"= "Right Wing",
-                               "rf"="Right Forward", "ram"="Right Attacking Midfielder", "rcm"="Right Center Midfielder",
-                               "rm"="Right Midfielder", "rdm"="Right Defensive Midfielder", "rcb"="Right Center Back",
-                               "rb"="Right Back", "rwb"="Right Wing Back", "st"="Striker", "lw"="Left Wing", "cf"="Center Forward",
-                               "cam"="Center Attacking Midfielder", "cm"="Center Midfielder", "lm"="Left Midfielder",
-                               "cdm"="Center Defensive Midfielder", "cb"="Center Back", "lb"="Left Back", "lwb"="Left Wing Back",
-                               "ls"="Left Striker", "lf"="Left Forward", "lam"="Left Attacking Midfielder", "lcm"="Left Center Midfielder",
-                               "ldm"="Left Defensive Midfielder", "lcb"="Left Center Back", "gk"="Goalkeeper"))
+
 
 # Name of positions
 positions <- select(raw.data, contains("prefers_")) %>% 
